6d32d64e09962a2df9f0e47903ae42b7402dc75e
Samuel Simões <samuelsimoes31@gmail.com>
2020-10-13 15:58:32 -0300

    📦 feat: add map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map,TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css'
 
 import mapMarkerImg from '../images/map-marker.svg'
 
@@ -24,7 +27,13 @@ function OrphanagesMap() {
           </footer>
         </aside>
 
-        <div></div>
+        <Map 
+          center={[-8.2921647,-35.0365562]}
+          zoom={15}
+          style={{width: '100%',height:'100%'}}
+        >
+          <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+        </Map>
 
         <Link to="" className="create-orphanage">
           <FiPlus size={32} color="#FFF"/>
