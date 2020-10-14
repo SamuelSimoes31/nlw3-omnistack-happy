44ac9ba8b9d3d70b6a8b72647008d19e16ec94ae
Samuel Simões <samuelsimoes31@gmail.com>
2020-10-14 20:09:27 -0300

    📦 feat: some configurations

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,13 @@
 import express, { response } from 'express';
 
+import './database/connection'
+
 const app = express();
 
+app.use(express.json());
+
 app.get('/users', (req,res) => {
-  return res.json({message: 'hello'})
+  return res.json({message: 'hello'});
 });
 
 app.listen(3333);
