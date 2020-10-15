b1595fe6a240319e5e61af37f31ebdf08247d99f
Samuel Simões <samuelsimoes31@gmail.com>
2020-10-15 12:06:35 -0300

    📦 feat: listing orphanages

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,7 +2,8 @@ import { Router } from 'express';
 import OrphanageController from './controllers/OrphanagesController'
 
 const routes = Router();
-
+//index, show, create, update, delete
+routes.get('/orphanages', OrphanageController.index);
 routes.post('/orphanages', OrphanageController.create);
 
 export default routes;
