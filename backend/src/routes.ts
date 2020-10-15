49b53101dea3c8a3f07d109b46cb9ddc5595c32c
Samuel Simões <samuelsimoes31@gmail.com>
2020-10-15 12:02:18 -0300

    📦 feat: controller added

---
+++ backend/src/routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import OrphanageController from './controllers/OrphanagesController'
+
+const routes = Router();
+
+routes.post('/orphanages', OrphanageController.create);
+
+export default routes;
