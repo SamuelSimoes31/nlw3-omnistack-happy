5728d34253695fa065d99b9533baf135565554e5
Samuel Simões <samuelsimoes31@gmail.com>
2020-10-15 18:12:22 -0300

    📦 feat: map marker

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map,TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map,TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet'
 
 import 'leaflet/dist/leaflet.css'
 
 import mapMarkerImg from '../images/map-marker.svg'
 
 import '../styles/pages/orphanages-map.css'
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58,68],
+  iconAnchor: [29,68],
+  popupAnchor: [170,2]
+})
 
 function OrphanagesMap() {
   return (
@@ -33,6 +41,19 @@ function OrphanagesMap() {
           style={{width: '100%',height:'100%'}}
         >
           <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+          <Marker
+            icon={mapIcon}
+            position={[-8.2921647,-35.0365562]}
+          >
+            <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+              Orfanato
+              <Link to="">
+                <FiArrowRight size={20} color="#fff"/>
+              </Link>
+            </Popup>
+          </Marker>
+
+
         </Map>
 
         <Link to="" className="create-orphanage">
