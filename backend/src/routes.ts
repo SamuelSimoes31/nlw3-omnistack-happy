be3c1241d19a9894ce708f68daa073d2c1634d93
Samuel Simões <samuelsimoes31@gmail.com>
2020-10-15 12:36:02 -0300

    📦 feat: upload images

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 import OrphanageController from './controllers/OrphanagesController'
 
+import uploadConfig from './config/upload';
+
 const routes = Router();
+const upload = multer(uploadConfig);
 //index, show, create, update, delete
 routes.get('/orphanages', OrphanageController.index);
 routes.get('/orphanages/:id', OrphanageController.show);
-routes.post('/orphanages', OrphanageController.create);
+routes.post('/orphanages', upload.array('images'),OrphanageController.create);
 
 export default routes;
