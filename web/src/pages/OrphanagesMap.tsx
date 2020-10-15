9e5aa7ca7fd0ddd5659150e10bd050864340a19d
Samuel Simões <samuelsimoes31@gmail.com>
2020-10-15 18:33:21 -0300

    📦 feat: pages CreateOrphanage, Orphanage. And Sidebar added

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -47,7 +47,7 @@ function OrphanagesMap() {
           >
             <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
               Orfanato
-              <Link to="">
+              <Link to="/orphanages/1">
                 <FiArrowRight size={20} color="#fff"/>
               </Link>
             </Popup>
@@ -56,7 +56,7 @@ function OrphanagesMap() {
 
         </Map>
 
-        <Link to="" className="create-orphanage">
+        <Link to="/orphanages/create" className="create-orphanage">
           <FiPlus size={32} color="#FFF"/>
         </Link>
       </div>
