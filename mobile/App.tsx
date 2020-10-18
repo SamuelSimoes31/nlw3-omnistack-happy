8d09916f8922720b853743e02eb8ac0733ff88ee
Samuel Simões <samuelsimoes31@gmail.com>
2020-10-17 21:47:22 -0300

    📦 feat: finished map screen style

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
 
 import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold, 
+    Nunito_700Bold, 
+    Nunito_800ExtraBold
+  });
+
+  if(!fontsLoaded){
+    return null
+  }
+
   return (
     <View style={styles.container}>
       <MapView
@@ -35,6 +48,15 @@ export default function App() {
           </Callout>
         </Marker>
       </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 Orfaananannana</Text>
+
+        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
+          <Feather name='plus' size={20} color='#fff'/>
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 }
@@ -51,15 +73,48 @@ const styles = StyleSheet.create({
 
   calloutContainer: {
     width: 160,
-    height: 46,
+    height: 56,
     paddingHorizontal: 16,
     backgroundColor: 'rgba(255,255,255,0.8)',
     borderRadius: 16,
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
 
   calloutText: {
+    fontFamily: 'Nunito_700Bold',
     color: '#0089a5',
     fontSize: 14
+  },
+
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    elevation: 3
+  },
+
+  footerText: {
+    fontFamily: 'Nunito_700Bold',
+    color: '#8fa7b3'
+  },
+
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
